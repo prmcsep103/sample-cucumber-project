47012492f0ce2c45597d762d4766da441ec36ada
prmcsep103 <primecommunity103@gmail.com>
2023-12-23 17:18:44 +0000

    Today's class update

--- src/test/java/com/nopcommerce/demo/runner/RunCukeTest.java
+++ src/test/java/com/nopcommerce/demo/runner/RunCukeTest.java
@@ -10,8 +10,9 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/resources/features",
         glue = "com/nopcommerce/demo/steps",
         plugin = {"html:target/cucumber-reports/cucumber.html",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        tags = "@regression and not @smoke"
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/cucumber-reports/cucumber.json"},
+        tags = "@smoke"
 )
 public class RunCukeTest extends AbstractTestNGCucumberTests {
 }
